d641b7c766564de9927b1e7a9fed34a672d989fa
ben-it-v2 <benoit.brunet@epitech.eu>
2024-06-04 19:24:30 -0400

    [UPDATE] Restaurant Page - Updated to allow restaurants search by name and/or by schedule

--- src/app/components/restaurant/restaurant.component.ts
+++ src/app/components/restaurant/restaurant.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api/api.service';
+import { SearchRestaurantsRequest } from 'src/app/services/api/requests/searchrestaurantsrequest';
+import { SearchRestaurantResponse } from 'src/app/services/api/responses/restaurantresponse';
 
 @Component({
   selector: 'app-restaurant',
   templateUrl: './restaurant.component.html',
   styleUrls: ['./restaurant.component.css']
 })
-export class RestaurantComponent {
+export class RestaurantComponent implements OnInit {
+  name: string = '';
+  restaurants: Array<SearchRestaurantResponse> = [];
+  timePickerFormat: number = 24;
+  timeFrom: string = '';
+  timeTo: string = '';
 
+  constructor(private apiService : ApiService) {}
+
+  ngOnInit(): void {}
+
+  searchRestaurants() {
+    let params: SearchRestaurantsRequest = {};
+
+    if (this.name !== '')
+      params.name = this.name;
+    if (this.timeFrom !== '') {
+      if (params.opened === undefined)
+        params.opened = {};
+      params.opened.from = `${this.timeFrom}:00`;
+    }
+    if (this.timeTo !== '') {
+      if (params.opened === undefined)
+        params.opened = {};
+      params.opened.to = `${this.timeTo}:00`;
+    }
+
+    this.apiService.searchRestaurants(params)
+      .subscribe(response => {
+        this.restaurants = response;
+      });
+  }
 }
