eacd12fe5a7f08508dbe3f7af03d1dc0262cf145
ben-it-v2 <benoit.brunet@epitech.eu>
2024-06-08 09:45:28 -0400

    [UPDATE] API Service - Added the create restaurant functionality

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environment/environment';
 import { Observable } from 'rxjs/internal/Observable';
 import { Subject } from 'rxjs/internal/Subject';
-import { HealthResponse } from './responses/healthresponse';
-import { SearchRestaurantResponse } from './responses/restaurantresponse';
-import { SearchRestaurantsRequest } from './requests/searchrestaurantsrequest';
+import { HealthResponse } from './responses/health.response';
+import { SearchRestaurantResponse } from './responses/searchrestaurant.response';
+import { SearchRestaurantsRequest } from './requests/searchrestaurants.request';
+import { CreateRestaurantRequest } from './requests/createrestaurant.request';
+import { CreateRestaurant } from './objects/createrestaurant';
 
 @Injectable({
   providedIn: 'root'
@@ -19,8 +21,27 @@ export class ApiService {
 
     this.http.get<HealthResponse>(ApiService.HEALTH_ENDPOINT)
       .subscribe(response => {
-        console.log(response);
         subject.next(response.status === ApiService.HEALTH_OK_STATUS);
+      });
+
+    return subject.asObservable();
+  }
+
+  createRestaurant(params: CreateRestaurant): Observable<boolean> {
+    let subject = new Subject<boolean>();
+    let body: CreateRestaurantRequest = {
+      name: params.name,
+      capacity: params.capacity,
+      hours: params.hours,
+      reservations: params.reservations
+    };
+
+    this.http.post(ApiService.RESTAURANT_ENDPOINT, body, {
+        headers: new HttpHeaders({
+        'owner': params.owner
+      })})
+      .subscribe(response => {
+        subject.next(true);
       });
 
     return subject.asObservable();
@@ -31,7 +52,7 @@ export class ApiService {
   }
 
   static HEALTH_ENDPOINT: string = `${environment.apiUrl}/health`;
-  static RESTAURANT_ENDPOINT: string = `${environment.apiUrl}/restaurant`;
+  static RESTAURANT_ENDPOINT: string = `${environment.apiUrl}/restaurants`;
   static SEARCH_RESTAURANTS_ENDPOINT: string = `${environment.apiUrl}/search/restaurants`;
   static HEALTH_OK_STATUS: string = 'ok';
 }
